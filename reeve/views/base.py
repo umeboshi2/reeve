d2f715012766b336396517916235a4ba45b72bc5
Joseph Rawson <joseph.rawson.works@littledebian.org>
2016-07-18 13:09:46 -0500

    added views

---
+++ reeve/views/base.py
@@ -0,0 +1,40 @@
+# basic view classes
+
+class BaseView(object):
+    def __init__(self, request):
+        self.request = request
+    
+    def get_app_settings(self):
+        return self.request.registry.settings
+
+class BaseUserView(BaseView):
+    def get_current_user_id(self):
+        "Get the user id quickly without db query"
+        if 'user' not in self.request.session:
+            return None
+        return self.request.session['user'].id
+
+    def get_current_user(self):
+        "Get user db object"
+        if 'user' not in self.request.session:
+            return None
+        db = self.request.db
+        user_id = self.request.session['user'].id
+        return db.query(self.usermodel).get(user_id)
+
+class BaseViewCallable(BaseView):
+    def __init__(self, request):
+        super(BaseViewCallable, self).__init__(request)
+        self.response = None
+        self.data = {}
+    
+    def __call__(self):
+        if self.response is not None:
+            return self.response
+        else:
+            return self.data
+
+class BaseUserViewCallable(BaseViewCallable, BaseUserView):
+    pass
+
+
---
+++ reeve/views/restviews.py
@@ -0,0 +1,49 @@
+import os
+from datetime import datetime
+
+import transaction
+from cornice.resource import resource, view
+
+
+from .base import BaseView
+
+def apiroot(prefix='/api', version='dev'):
+    return os.path.join(prefix, version)
+
+
+class BaseResource(BaseView):
+    def __init__(self, request):
+        super(BaseResource, self).__init__(request)
+        self.db = self.request.db
+        self.limit = None
+        self.max_limit = 100
+        
+    def serialize_object(self, dbobj):
+        return dbobj.serialize()
+
+    # use this when you do only need  certain model attributes
+    # in a collection, rather than return the complete models
+    def serialize_object_for_collection_query(self, dbobj):
+        return self.serialize_object(dbobj)
+
+    def collection_query(self):
+        raise RuntimeError, "Implement me in subclass"
+
+    def collection_get(self):
+        offset = 0
+        limit = self.limit
+        GET = self.request.GET
+        if 'offset' in GET:
+            offset = int(GET['offset'])
+        if 'limit' in GET:
+            limit = int(GET['limit'])
+            if limit > self.max_limit:
+                limit = self.max_limit
+        q = self.collection_query()
+        total_count = q.count()
+        q = q.offset(offset).limit(limit)
+        objects = q.all()
+        data = [self.serialize_object_for_collection_query(o) for o in objects]
+        return dict(total_count=total_count, data=data)
+    
+
